d09609ff8c042c49fa5580107802c820e5a8780f
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-03-23 15:26:10 +0500

    Update Stripe.js

--- Stripe.js
+++ Stripe.js
@@ -5,3 +5,14 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET);
         name: userNew.firstname,
         email: userNew.email,
       });
+
+//Then update the customer with the new stripe id
+let updatedUser = await SignUp.update(
+        {
+          customer_stripe_id: customer.id,
+        },
+        {
+          where: { id: Number(userNew.dataValues.id) },
+          returning: true,
+        }
+      );
